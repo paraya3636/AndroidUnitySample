d7caaf3848caf4a28407f829ef22c610ed2e7598
Paraya <paraya3636@users.noreply.github.com>
2019-04-26 15:59:20 +0900

    namespace消す

--- Assets/Editor/MyBuilder.cs
+++ Assets/Editor/MyBuilder.cs
@@ -1,27 +1,24 @@
 using System.Collections.Generic;
 using UnityEditor;
 
-namespace Editor
-{
-    public class MyBuilder {
-        // ビルド実行でAndroidのapkを作成する例
-        [UnityEditor.MenuItem("Tools/Build Project AllScene Android")]
-        public static void BuildProjectAllSceneAndroid() {
-            EditorUserBuildSettings.SwitchActiveBuildTarget( BuildTarget.Android );
-            List<string> allScene = new List<string>();
-            foreach( EditorBuildSettingsScene scene in EditorBuildSettings.scenes ){
-                if (scene.enabled) {
-                    allScene.Add (scene.path);
-                }
-            }   
+public class MyBuilder {
+    // ビルド実行でAndroidのapkを作成する例
+    [UnityEditor.MenuItem("Tools/Build Project AllScene Android")]
+    public static void BuildProjectAllSceneAndroid() {
+        EditorUserBuildSettings.SwitchActiveBuildTarget( BuildTarget.Android );
+        List<string> allScene = new List<string>();
+        foreach( EditorBuildSettingsScene scene in EditorBuildSettings.scenes ){
+            if (scene.enabled) {
+                allScene.Add (scene.path);
+            }
+        }   
 //            PlayerSettings.bundleIdentifier = "sample.aiueo";
-            PlayerSettings.statusBarHidden = true;
-            BuildPipeline.BuildPlayer( 
-                allScene.ToArray(),
-                "newgame.apk",
-                BuildTarget.Android,
-                BuildOptions.None
-            );
-        }
+        PlayerSettings.statusBarHidden = true;
+        BuildPipeline.BuildPlayer( 
+            allScene.ToArray(),
+            "newgame.apk",
+            BuildTarget.Android,
+            BuildOptions.None
+        );
     }
 }
